d8cdb60b2310fa23b1804815dbd9fb233070bc99
zenin5479 <zenin5479@gmail.com>
2026-05-17 16:15:21 +0300

    Update 0.3 Обновлен метод. Проведен рефакторинг

---
+++ SharedLibrary/Library.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace SharedLibrary
+{
+   public class CalculationRequest
+   {
+      public string Operation { get; set; }          // "Sum", "Product", "Average" и т.п.
+      public List<double> Numbers { get; set; }
+      public RequestParameters Parameters { get; set; }
+   }
+
+   public class RequestParameters
+   {
+      public bool RoundResult { get; set; }
+      public int Precision { get; set; } = 2;
+   }
+
+   public class CalculationResponse
+   {
+      public double Result { get; set; }
+      public List<string> Steps { get; set; } = new List<string>();
+      public bool Success { get; set; }
+      public string ErrorMessage { get; set; }
+   }
+}
--- SharedLibrary/RequestParameters.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SharedLibrary
-{
-   public class RequestParameters
-   {
-      public bool RoundResult { get; set; }
-      public int Precision { get; set; } = 2;
-   }
-}
