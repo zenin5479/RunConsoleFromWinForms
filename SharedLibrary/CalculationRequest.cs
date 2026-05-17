e6a7878f170b9d2f90fd9db9ddd95301687f2341
zenin5479 <zenin5479@gmail.com>
2026-05-17 21:28:28 +0300

    Update 2.3 Обновлен метод. Проведен рефакторинг

---
+++ SharedLibrary/CalculationRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SharedLibrary
+{
+   public class CalculationRequest
+   {
+      public string Operation { get; set; }
+      public List<double> Numbers { get; set; }
+      public RequestParameters Parameters { get; set; }
+   }
+}
---
+++ SharedLibrary/CalculationResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace SharedLibrary
+{
+   public class CalculationResponse
+   {
+      public double Result { get; set; }
+      public List<string> Steps { get; set; } = new List<string>();
+      public bool Success { get; set; }
+      public string ErrorMessage { get; set; }
+   }
+}
