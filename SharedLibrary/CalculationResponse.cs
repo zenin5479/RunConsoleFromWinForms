d45e634f4b6baf50c0af6faefb02cd755d2292a8
zenin5479 <zenin5479@gmail.com>
2026-05-17 16:06:53 +0300

    Добавьте файлы проекта.

---
+++ SharedLibrary/CalculationResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SharedLibrary
+{
+   public class CalculationResponse
+   {
+      public double Result { get; set; }
+      public List<string> Steps { get; set; } = new List<string>();
+      public bool Success { get; set; }
+      public string ErrorMessage { get; set; }
+   }
+}
---
+++ SharedLibrary/RequestParameters.cs
@@ -0,0 +1,8 @@
+﻿namespace SharedLibrary
+{
+   public class RequestParameters
+   {
+      public bool RoundResult { get; set; }
+      public int Precision { get; set; } = 2;
+   }
+}
