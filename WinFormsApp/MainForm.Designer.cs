f480f4f67b20725fcbf2d399ed2d01b02c987e3e
zenin5479 <zenin5479@gmail.com>
2026-05-19 11:31:59 +0300

    Update 4.9 Обновлен метод. Проведен рефакторинг

--- WinFormsApp/MainForm.Designer.cs
+++ WinFormsApp/MainForm.Designer.cs
@@ -98,9 +98,9 @@
          labelOperation.AutoSize = true;
          labelOperation.Location = new System.Drawing.Point(12, 9);
          labelOperation.Name = "labelOperation";
-         labelOperation.Size = new System.Drawing.Size(58, 15);
+         labelOperation.Size = new System.Drawing.Size(61, 15);
          labelOperation.TabIndex = 6;
-         labelOperation.Text = "Действие";
+         labelOperation.Text = "Действие:";
          // 
          // labelNumbers
          // 
